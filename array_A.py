a7abed28fa7a4cfd0e4d067f705f8b243de20a3a
PortovArtem <59965549+PortovArtem@users.noreply.github.com>
2020-01-16 17:10:06 +0300

    uploaded file

---
+++ array_A.py
@@ -0,0 +1,22 @@
+def even(string):
+    string += ' '
+    A=[]
+    temp_str = ''
+
+    for i in range(len(string)):
+
+        if string[i] != ' ':
+            temp_str += string[i]
+        else:
+            A.append(temp_str)
+            temp_str = ''
+        # print(temp_str)
+
+    for i in range(len(A)):
+        A[i] = int(A[i])
+
+    result = ''
+    for i in range(len(A)):
+        if i%2==0:
+           result+= str(A[i]) + ' '
+    return result
